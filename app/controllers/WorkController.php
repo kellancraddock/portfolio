a5c420c99ed01cb5da2e40db5b51cb3c2da1df6f
Kellan Craddock <kellancraddock@kellan-craddocks-macbook-pro.local>
2009-08-16 20:32:49 -0700

    added work controller and view

---
+++ app/controllers/WorkController.php
@@ -0,0 +1,14 @@
+<?php
+	class WorkController extends Zend_Controller_Action
+	{
+		public function init() {
+			//$this->session_alert = new Zend_Session_Namespace('');
+			//$this->Model = new Model();
+		}
+
+		public function indexAction()
+		{
+				
+		}
+	}
+?>

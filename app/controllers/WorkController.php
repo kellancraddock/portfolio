20fb6a8eeb7db74c928a36dd892e8c4c8feb17d9
Kellan Craddock <kellancraddock@kellan-craddocks-macbook-pro.local>
2009-09-06 03:29:05 -0700

    added project action to handle all aspects of project view

--- app/controllers/WorkController.php
+++ app/controllers/WorkController.php
@@ -1,4 +1,7 @@
 <?php
+	require_once("../app/models/ImageModel.php");
+	require_once("../app/models/ProjectModel.php");
+	require_once("../app/models/ContributionModel.php");
 	class WorkController extends Zend_Controller_Action
 	{
 		public function init() {
@@ -9,7 +12,48 @@
 
 		public function indexAction()
 		{
-				
+			header("Location: /work/project");
 		}
+		
+		public function projectAction()
+		{
+			$image_model = new ImageModel();
+			$project_model = new ProjectModel();
+			$contribution_model = new ContributionModel();
+			
+			$active_projects = $project_model->getActive();
+			
+			//Build thumbs array from active projects
+			$thumbs = array();
+			foreach ($active_projects as $project) {
+				$thumbs[] = $image_model->getThumb($project['id']);
+			}
+			
+			$this->view->thumbs = $thumbs;
+			
+			$project_id = $this->_request->getParam('id');
+			$view_id = $this->_request->getParam('view');
+			
+			if (!$project_id) {
+				$thumbs = reset($thumbs);
+				$project_id = $thumbs['project_id'];
+			}
+			
+			$this->project = $project_model->getOne($project_id);
+			$this->project['contributions'] = $contribution_model->getOne($project_id);
+			$this->project['images'] = $image_model->getAll($project_id);
+			if (isset($view_id)) {
+				foreach ($this->project['images'] as $image) {
+					if ($image['id'] == $view_id) {
+						$this->project['view'] = $image['file_name'];
+					}
+				}
+			} else {
+				$this->project['view'] = $this->project['images']['image1']['file_name'];
+			}
+			
+			$this->view->project = $this->project;
+		}
+		
 	}
 ?>

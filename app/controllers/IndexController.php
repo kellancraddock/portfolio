58e69967788717e66bac0578a9683e2f7d71a98b
Kellan Craddock <kellancraddock@kellan-craddocks-macbook-pro.local>
2009-09-06 21:38:31 -0700

    pulling in project info and passing it to view

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,4 +1,6 @@
 <?php
+	require_once("../app/models/ImageModel.php");
+	require_once("../app/models/ProjectModel.php");
 	class IndexController extends Zend_Controller_Action
 	{
 		public function init() {
@@ -9,7 +11,20 @@
 		
 		public function indexAction()
 		{
+			$image_model = new ImageModel();
+			$project_model = new ProjectModel();
+			
+			$project_id = $this->_request->getParam('id');
+			//If the project id isnt set, get a random one
+			if (!$project_id) {
+				$active_projects = $project_model->getActive();
+				$project_id = $active_projects[array_rand($active_projects)]['id'];
+			}
+			
+			$this->project = $project_model->getOne($project_id);
+			$this->project['image'] = $image_model->getPrimary($project_id);
 
+			$this->view->project = $this->project;
 		}
 		
 	}

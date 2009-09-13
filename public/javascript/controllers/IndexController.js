c6b9fd0c6fdeadf162d62d312832e6e8c52e992f
Kellan Craddock <kellancraddock@kellan-craddocks-macbook-pro.local>
2009-09-13 02:09:06 -0700

    modified controller to add view properties and bind events

--- public/javascript/controllers/IndexController.js
+++ public/javascript/controllers/IndexController.js
@@ -4,16 +4,37 @@ function IndexController() {
 	var self = this;
 	this.interval;
 	this.slideshow_model;
+	this.gallery;
 	
 	//Constructor
 	this.construct = function() {
 		self.slideshow_model = new SlideShowModel();
 		self.slideshowAction();
 		
+		self.gallery = $('#gallery .project_images');
+		
+		//Set view variables
+		self.view.gallery = self.gallery;
+		self.view.slide_width = 1000;
+		self.view.off_set = 0;
+		self.view.slideshow_setwidth();
+		
+		//reset control position
+		self.view.controls_move({direction : 'up'});
+		
 		$('#gallery').bind('mouseover', function() {
-			self.stopAction();	
+			self.controlsAction.pause();	
 		}).bind('mouseout', function() {
-			self.startAction();
+			self.view.controls_move({direction : 'up'});
+			self.controlsAction.auto();
+		});
+		
+		$('.left a', '.controls_wrapper').bind('click', function() {
+			self.controlsAction.prev();
+		});
+		
+		$('.right a', '.controls_wrapper').bind('click', function() {
+			self.controlsAction.next();
 		});
 	}
 	
@@ -26,32 +47,65 @@ function IndexController() {
 			location : 'index/images', 
 		}, function(response) {
 			if(self.view.slideshow_load(response)) {
-				self.startAction();
+				self.controlsAction.auto();
 			} else {
 				self.view.alert('error', 'Error Loading Projects');
 			};
 		});
 	}
 	
-	this.startAction = function() {
-		//Set SlideShow interval
-		self.interval = setInterval(function() {
-			var new_project = self.view.slideshow_move();
-			(new_project) ? self.updateInfoAction(new_project) : self.updateInfoAction(self.view.slideshow_reset());
-		}, 4000);
+	this.controlsAction = {
+		auto: function() {
+				//Set SlideShow interval
+				self.interval = setInterval(function() {
+					self.view.controls_move({direction : 'down'});
+					setTimeout(function() {
+						if ($('.active', self.gallery).next('li').length) {
+							self.view.slideshow_resetactive($('.active', self.gallery).next('li'));
+							var new_project = self.view.slideshow_next();
+						}
+						(new_project) ? self.updateInfoAction.auto(new_project) : self.updateInfoAction.auto(self.view.slideshow_reset());
+					}, 800);
+				}, 4000);
+			},
+		next: function() {
+				if ($('.active', self.gallery).next('li').length) {
+					self.view.slideshow_resetactive($('.active', self.gallery).next('li'));
+					var new_project = self.view.slideshow_next();
+				}
+				(new_project) ? self.updateInfoAction.manual(new_project) : self.updateInfoAction.manual(self.view.slideshow_reset());
+			},
+		prev: function() {
+				if ($('.active', self.gallery).prev('li').length) {
+					self.view.slideshow_resetactive($('.active', self.gallery).prev('li'));
+					var new_project = self.view.slideshow_prev();
+				}
+				(new_project) ? self.updateInfoAction.manual(new_project) : self.updateInfoAction.manual(self.view.slideshow_reset());
+			},
+		pause: function() {
+			clearInterval(self.interval);
+			self.view.controls_move({direction : 'open'});
+			}
 	}
 	
-	this.stopAction = function() {
-		clearInterval(self.interval);
-	}
-	
-	this.updateInfoAction = function(id) {
-		self.slideshow_model.getOne({
-			data : 'id=' + id,
-			location : 'index/info',
-		}, function(response) {
-			self.view.updateControlls(response);
-		});
+	this.updateInfoAction = {
+		auto: function(id) {
+			self.slideshow_model.getOne({
+				data : 'id=' + id,
+				location : 'index/info',
+			}, function(response) {
+				self.view.controls_update(response);
+				self.view.controls_move({direction : 'up'});
+			});
+		},
+		manual: function(id) {
+			self.slideshow_model.getOne({
+				data : 'id=' + id,
+				location : 'index/info',
+			}, function(response) {
+				self.view.controls_update(response);
+			});
+		}
 	}
 	
 	self.construct();

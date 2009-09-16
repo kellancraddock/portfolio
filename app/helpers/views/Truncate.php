4cc423c145ba57158007c21f7f0fe14eb3f69a72
Kellan Craddock <kellancraddock@kellan-craddocks-macbook-pro.local>
2009-09-16 02:58:05 -0700

    added extra params in helper

--- app/helpers/views/Truncate.php
+++ app/helpers/views/Truncate.php
@@ -1,11 +1,12 @@
 <?php
 	class View_Helper_Truncate extends Zend_Controller_Action_Helper_Abstract
 	{
-		public function truncate($text, $limit) {
-      		if (str_word_count($text) > $limit) {
+		public function truncate($text, $options) {
+      		if (str_word_count($text) > $options['limit']) {
 	          	$words = str_word_count($text, 2);
 	         	$pos = array_keys($words);
-	          	$text = substr($text, 0, $pos[$limit]) . '...';
+	         	$url = (isset($options['url'])) ? '<a href="' . $options['url'] . '">More</a>' : '';
+	          	$text = substr($text, 0, $pos[$options['limit']]) . "... {$url}";
       		}
       		return $text;
     	}

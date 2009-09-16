a9af49f0e7b7a710a5385a6a4741c73af626a9ed
Kellan Craddock <kellancraddock@kellan-craddocks-macbook-pro.local>
2009-09-16 02:57:33 -0700

    added mail controller

--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -10,6 +10,29 @@
 		{
 				
 		}
+		
+		public function mailAction()
+		{
+			$postArray = array(
+				'Name'=>$_POST['name'],
+				'Email'=>$_POST['email'],
+				'Message'=>$_POST['message']
+			);
+						
+			//Mail Helper
+			$message_helper = $this->_helper->Message;
+			$errors = $message_helper->validate($postArray);
+
+			if ($errors) {
+				$this->view->mail_status = array('status' => 'fail', 'message' => $errors);	
+			} else {
+				$message_helper->sendMessage($postArray);
+				$this->view->mail_status = array('status' => 'success', 'message' => 'Thanks for the message!');
+			}
+				
+			header( "Location: /contact" );
+		}
+
 	}
 	
 ?>
